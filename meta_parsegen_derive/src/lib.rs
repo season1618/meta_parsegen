60682c496a9d12eab1bc0bfca60b9e085bac885c
season1618 <seasonminority314@gmail.com>
2024-03-09 07:37:42 +0900

    refactor

--- meta_parsegen_derive/src/lib.rs
+++ meta_parsegen_derive/src/lib.rs
@@ -2,28 +2,32 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, DeriveInput, Data, Ident, Type};
+use syn::{self, DeriveInput, Data, Fields, Ident, Type};
 use Data::*;
+use Fields::*;
 
 #[proc_macro_derive(Parser)]
 pub fn parse_derive(tokens: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(tokens).unwrap();
     let name = ast.ident;
     let gen = match ast.data {
-        Struct(data_struct) => {
-            let (idents, types): (Vec<Ident>, Vec<Type>) = data_struct.fields
-                .into_iter()
-                .map(|field| (field.ident.unwrap(), field.ty))
-                .unzip();
-
-            quote! {
-                impl Parser for #name {
-                    fn parse(s: &str) -> (&str, #name) {
-                        #(let (s, #idents) = #types::parse(s);)*
-                        (s, #name { #(#idents),* })
+        Struct(data_struct) => match data_struct.fields {
+            Named(fields) => {
+                let (idents, types): (Vec<Ident>, Vec<Type>) = fields.named
+                    .into_iter()
+                    .map(|field| (field.ident.unwrap(), field.ty))
+                    .unzip();
+                
+                quote! {
+                    impl Parser for #name {
+                        fn parse(s: &str) -> (&str, #name) {
+                            #(let (s, #idents) = #types::parse(s);)*
+                            (s, #name { #(#idents),* })
+                        }
                     }
                 }
-            }
+            },
+            _ => panic!(),
         },
         _ => panic!(),
     };
