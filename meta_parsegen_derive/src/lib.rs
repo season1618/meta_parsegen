6b141556298b21ccd1f398872efe1ee5e78a0f2c
season1618 <seasonminority314@gmail.com>
2024-03-09 08:52:12 +0900

    extract 'parse_sequence_derive'

--- meta_parsegen_derive/src/lib.rs
+++ meta_parsegen_derive/src/lib.rs
@@ -1,6 +1,5 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
 use syn::{self, DeriveInput, Data, Fields, Ident, Type};
@@ -8,52 +7,56 @@ use Data::*;
 use Fields::*;
 
 #[proc_macro_derive(Parser)]
-pub fn parse_derive(tokens: TokenStream) -> TokenStream {
+pub fn parse_derive(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast: DeriveInput = syn::parse(tokens).unwrap();
     let name = ast.ident;
     let gen = match ast.data {
-        Struct(data_struct) => match data_struct.fields {
-            Named(fields) => {
-                let (idents, types): (Vec<Ident>, Vec<Type>) = fields.named
-                    .into_iter()
-                    .map(|field| (field.ident.unwrap(), field.ty))
-                    .unzip();
-                
-                quote! {
-                    impl Parser for #name {
-                        fn parse(s: &str) -> Option<(&str, #name)> {
-                            #(let (s, #idents) = #types::parse(s)?;)*
-                            Some((s, #name { #(#idents),* }))
-                        }
-                    }
-                }
-            },
-            Unnamed(fields) => {
-                let (idents, types): (Vec<Ident>, Vec<Type>) = fields.unnamed
-                    .into_iter().enumerate()
-                    .map(|(i, field)| (Ident::new(&format!("ident{}", i), Span::call_site()), field.ty))
-                    .unzip();
-                
-                quote! {
-                    impl Parser for #name {
-                        fn parse(s: &str) -> Option<(&str, #name)> {
-                            #(let (s, #idents) = #types::parse(s)?;)*
-                            Some((s, #name ( #(#idents),* )))
-                        }
-                    }
-                }
-            },
-            Unit => {
-                quote! {
-                    impl Parser for #name {
-                        fn parse(s: &str) -> Option<(&str, #name)> {
-                            Some((s, #name))
-                        }
-                    }
-                }
-            },
-        },
+        Struct(data) => parse_sequence_derive(name, data.fields),
         _ => panic!(),
     };
     gen.into()
+}
+
+fn parse_sequence_derive(name: Ident, fields: Fields) -> proc_macro2::TokenStream {
+    match fields {
+        Named(fields) => {
+            let (idents, types): (Vec<Ident>, Vec<Type>) = fields.named
+                .into_iter()
+                .map(|field| (field.ident.unwrap(), field.ty))
+                .unzip();
+            
+            quote! {
+                impl Parser for #name {
+                    fn parse(s: &str) -> Option<(&str, #name)> {
+                        #(let (s, #idents) = #types::parse(s)?;)*
+                        Some((s, #name { #(#idents),* }))
+                    }
+                }
+            }
+        },
+        Unnamed(fields) => {
+            let (idents, types): (Vec<Ident>, Vec<Type>) = fields.unnamed
+                .into_iter().enumerate()
+                .map(|(i, field)| (Ident::new(&format!("ident{}", i), Span::call_site()), field.ty))
+                .unzip();
+            
+            quote! {
+                impl Parser for #name {
+                    fn parse(s: &str) -> Option<(&str, #name)> {
+                        #(let (s, #idents) = #types::parse(s)?;)*
+                        Some((s, #name ( #(#idents),* )))
+                    }
+                }
+            }
+        },
+        Unit => {
+            quote! {
+                impl Parser for #name {
+                    fn parse(s: &str) -> Option<(&str, #name)> {
+                        Some((s, #name))
+                    }
+                }
+            }
+        },
+    }
 }
