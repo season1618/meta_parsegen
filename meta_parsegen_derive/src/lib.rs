b5fb79d81c3472bbf60a94df6cc4080a1b18ed3c
season1618 <seasonminority314@gmail.com>
2024-03-09 09:06:33 +0900

    fix

--- meta_parsegen_derive/src/lib.rs
+++ meta_parsegen_derive/src/lib.rs
@@ -11,27 +11,33 @@ pub fn parse_derive(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream 
     let ast: DeriveInput = syn::parse(tokens).unwrap();
     let name = ast.ident;
     let gen = match ast.data {
-        Struct(data) => parse_sequence_derive(name, data.fields),
+        Struct(data) => {
+            let sequence = parse_sequence_derive(&name, data.fields);
+
+            quote! {
+                impl Parser for #name {
+                    fn parse(s: &str) -> Option<(&str, #name)> {
+                        #sequence
+                    }
+                }
+            }
+        },
         _ => panic!(),
     };
     gen.into()
 }
 
-fn parse_sequence_derive(name: Ident, fields: Fields) -> proc_macro2::TokenStream {
+fn parse_sequence_derive(name: &Ident, fields: Fields) -> proc_macro2::TokenStream {
     match fields {
         Named(fields) => {
             let (idents, types): (Vec<Ident>, Vec<Type>) = fields.named
                 .into_iter()
                 .map(|field| (field.ident.unwrap(), field.ty))
                 .unzip();
-            
+
             quote! {
-                impl Parser for #name {
-                    fn parse(s: &str) -> Option<(&str, #name)> {
-                        #(let (s, #idents) = #types::parse(s)?;)*
-                        Some((s, #name { #(#idents),* }))
-                    }
-                }
+                #(let (s, #idents) = #types::parse(s)?;)*
+                Some((s, #name { #(#idents),* }))
             }
         },
         Unnamed(fields) => {
@@ -39,24 +45,14 @@ fn parse_sequence_derive(name: Ident, fields: Fields) -> proc_macro2::TokenStrea
                 .into_iter().enumerate()
                 .map(|(i, field)| (Ident::new(&format!("ident{}", i), Span::call_site()), field.ty))
                 .unzip();
-            
+
             quote! {
-                impl Parser for #name {
-                    fn parse(s: &str) -> Option<(&str, #name)> {
-                        #(let (s, #idents) = #types::parse(s)?;)*
-                        Some((s, #name ( #(#idents),* )))
-                    }
-                }
+                #(let (s, #idents) = #types::parse(s)?;)*
+                Some((s, #name ( #(#idents),* )))
             }
         },
         Unit => {
-            quote! {
-                impl Parser for #name {
-                    fn parse(s: &str) -> Option<(&str, #name)> {
-                        Some((s, #name))
-                    }
-                }
-            }
+            quote! { Some((s, #name)) }
         },
     }
 }
