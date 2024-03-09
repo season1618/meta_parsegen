e6f1cbfd87511f69e92065ac8c0ae91048d24243
season1618 <seasonminority314@gmail.com>
2024-03-09 12:50:35 +0900

    enum (unnamed variant)

--- meta_parsegen_derive/src/lib.rs
+++ meta_parsegen_derive/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro2::Span;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{self, DeriveInput, Data, Fields, Ident, Type};
 use Data::*;
 use Fields::*;
@@ -19,6 +19,44 @@ pub fn parse_derive(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream 
                     fn parse(s: &str) -> Option<(&str, #name)> {
                         #sequence
                     }
+                }
+            }
+        },
+        Enum(data) => {
+            let (methods, parse_vars): (Vec<Ident>, Vec<proc_macro2::TokenStream>) = data.variants
+                .into_iter()
+                .map(|variant| {
+                    let tag = variant.ident;
+                    let fields = match variant.fields {
+                        Unnamed(fields) => fields,
+                        _ => panic!(),
+                    };
+                    let (idents, types): (Vec<Ident>, Vec<Type>) = fields.unnamed
+                        .into_iter().enumerate()
+                        .map(|(i, field)| (Ident::new(&format!("ident{}", i), Span::call_site()), field.ty))
+                        .unzip();
+
+                    let code = quote! {
+                        #(let (s, #idents) = #types::parse(s)?;)*
+                        Some((s, #name::#tag( #(#idents),* )))
+                    };
+
+                    (format_ident!("parse_{}", tag), code)
+                })
+                .unzip();
+
+            quote! {
+                impl Parser for #name {
+                    fn parse(s: &str) -> Option<(&str, #name)> {
+                        #(if let Some(res) = #name::#methods(s) { return Some(res); })*
+                        None
+                    }
+                }
+
+                impl #name {
+                    #(fn #methods(s: &str) -> Option<(&str, #name)> {
+                        #parse_vars
+                    })*
                 }
             }
         },
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,15 @@ mod tests {
     use meta_parsegen_derive::Parser;
 
     #[derive(Debug, PartialEq, Parser)]
+    struct AorB2(AorB, AorB);
+
+    #[derive(Debug, PartialEq, Parser)]
+    enum AorB {
+        ItemA(A),
+        ItemB(B),
+    }
+
+    #[derive(Debug, PartialEq, Parser)]
     struct AA(A, A);
 
     #[derive(Debug, PartialEq, Parser)]
@@ -16,11 +25,25 @@ mod tests {
     #[derive(Debug, PartialEq)]
     struct A;
 
+    #[derive(Debug, PartialEq)]
+    struct B;
+
     impl Parser for A {
         fn parse(s: &str) -> Option<(&str, Self)> {
             let mut chs = s.chars();
             if Some('a') == chs.next() {
                 Some((chs.as_str(), A))
+            } else {
+                None
+            }
+        }
+    }
+
+    impl Parser for B {
+        fn parse(s: &str) -> Option<(&str, Self)> {
+            let mut chs = s.chars();
+            if Some('b') == chs.next() {
+                Some((chs.as_str(), B))
             } else {
                 None
             }
@@ -36,5 +59,8 @@ mod tests {
         assert_eq!(AA::parse("bbc"), None);
         assert_eq!(AA2::parse("aab"), Some(("b", AA2 { a: A, a2: A })));
         assert_eq!(AA2::parse("bbc"), None);
+        assert_eq!(AorB::parse("aac"), Some(("ac", AorB::ItemA(A))));
+        assert_eq!(AorB::parse("bbc"), Some(("bc", AorB::ItemB(B))));
+        assert_eq!(AorB2::parse("abc"), Some(("c", AorB2(AorB::ItemA(A), AorB::ItemB(B)))));
     }
 }
